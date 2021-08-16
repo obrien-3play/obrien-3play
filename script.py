7401d2564dbabc902c75b4b9ccf8306a2a7e5eb6
obrien-3play <88346620+obrien-3play@users.noreply.github.com>
2021-08-16 15:37:27 -0400

    Add files via upload
    Python script

---
+++ script.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+import os 
+import os.path 
+import errno
+import shutil
+
+#print("Enter folder path")
+#given_dir = os.path.abspath(input())
+
+
+path= '/Users/otenkeu/error_dir'
+
+# for storing size of each file
+size = 0
+
+max_size = 131072
+
+given_dir = '~/otenkeu/Downloads'
+
+
+
+# Check whether the specified path is an existing directory or not
+
+if not os.path.exists(path):
+    try:
+        os.makedirs(path)  
+    except OSError as error:
+        if error.errno != errno.EEXIST:
+            raise
+        
+for path, dirs, files in os.walk(path):
+        # checking the size of each file
+    for file in files:
+        size = os.stat(os.path.join( folder, file  )).st_size
+        
+        if size>max_size:
+            shutil.move(path.join(given_dir,file), path)
+                      
+        print ('Script ran successfully')
